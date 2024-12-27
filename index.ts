772bccc28a08d9fb3bd5a45da66807a286e4bc27
ImadNait <naitmihoubimzd@gmail.com>
2024-12-27 18:54:39 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { Elysia, t } from "elysia"
+const PORT = process.env.PORT || 4000;
+const app = new Elysia()
+.get("/",()=>{
+    return new Response(Bun.file('./public/index.html'))
+})
+.post("/shorter",({body})=>{
+
+return"short URL"
+
+}, {
+    body:t.Object({
+        url:t.String()
+    })
+})
+
+.get("/shorter/:id",({params})=>{
+    return "shorten url"
+})
+
+
+
+
+
+
+
+
+.listen(PORT)
+console.log(`Server running on port: ${PORT}`)
