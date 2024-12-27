e9093216990c17716dd6404fd5cd1bc14181d5f0
ImadNait <naitmihoubimzd@gmail.com>
2024-12-27 22:49:03 +0100

    fixing issues

--- index.ts
+++ index.ts
@@ -1,45 +1,51 @@
-import { Elysia, redirect, t } from 'elysia';
-import mongoose, { type ConnectOptions } from 'mongoose';
+import { Elysia, redirect } from 'elysia';
+import mongoose from 'mongoose';
 import ShortUrlModel from './database/schema';
 const PORT = process.env.PORT || 4000;
 
+mongoose.connect('mongodb://localhost:27017/URL/url',{ 
+    autoIndex: true
+  }).then(() => {
+  console.log("Connected to Mongo Database");
+}).catch((err) => {
+  console.error(`Error: ${err}`);
+});
 
-mongoose.connect('mongodb://localhost/urlShortener').then(()=>{
-    console.log("Connected to Mongo Database");
-}).catch((err)=>{
-    return `Error:${err}`
-})
+interface RequestBody {
+  fullUrl: string;
+}
 
 const app = new Elysia()
-.get("/",()=>{
+  .get("/", () => {
     return new Response(Bun.file('./public/index.html'))
-})
+  })
 
-.post("/shorter",({body: {url}})=>{
-    if(!url){
-        throw new Error("URL is required!")
+  .post("/shorter", async ({ body }: { body: RequestBody }) => {  // Type the body here
+    const { fullUrl } = body;  // Now TypeScript knows fullUrl exists
+
+    if (!fullUrl) {
+      throw new Error("URL is required!");
     }
-    ShortUrlModel.create({ fullURL: url })
-    redirect('/');
-    console.log(url);
 
+    const shorturl = new ShortUrlModel({ fullURL: fullUrl });
+    await shorturl.save();
+    console.log(shorturl.shortURL);
 
-}, {
-    body:t.Object({
-        url:t.String()
-    })
-})
-.get("/shorter/:id",({params})=>{
-    return "shorten url"
-})
-.get("/short/url",(url)=>{
-    return {url}
-})
+    return { shortURL: `http://localhost:4000/${shorturl.shortURL}` };
+  })
 
+  .get("/:shorturl", async ({ params }) => {
+    const { shorturl } = params;
 
+    const shortUrlDoc = await ShortUrlModel.findOne({ shortURL: shorturl });
+    
+    if (!shortUrlDoc) {
+      return { error: "Short URL not found" };
+    }
+    
+    return redirect(shortUrlDoc.fullURL);  // Proper redirection
+  })
+  
+  .listen(PORT);
 
-
-
-
-.listen(PORT)
-console.log(`Server running on port: ${PORT}`)
+console.log(`Server running on port: ${PORT}`);
